a746f4b7431d1e23bb27e4613264f79320b567c1
Ivano Dalmasso <ivano.dalmasso@gmail.com>
2021-02-04 00:54:20 +0100

    c51171c

---
+++ precache-manifest.050989e1f1784f66cf23ca41629b02a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca455503ddb316e369f",
+    "url": "css/app.396f4c28.css"
+  },
+  {
+    "revision": "fe5e6630474b50c2ed5ed2ca48091de4",
+    "url": "index.html"
+  },
+  {
+    "revision": "6ca455503ddb316e369f",
+    "url": "js/app.3d941563.js"
+  },
+  {
+    "revision": "48cc188c0dffbc0568d7",
+    "url": "js/chunk-vendors.593c8402.js"
+  },
+  {
+    "revision": "f936c5fa927a1c09d0737c301f29341d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.050989e1f1784f66cf23ca41629b02a4.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "go-vue-tutorial-frontend"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/api\/posts/, new workbox.strategies.NetworkOnly({ plugins: [new workbox.backgroundSync.Plugin("my-queue-name", { maxRetentionTime: 86400 })] }), 'POST');
+workbox.routing.registerRoute(/api\/posts/, new workbox.strategies.NetworkOnly({ plugins: [new workbox.backgroundSync.Plugin("my-queue-name", { maxRetentionTime: 86400 })] }), 'DELETE');
+workbox.routing.registerRoute(/api\/posts/, new workbox.strategies.NetworkOnly({ plugins: [new workbox.backgroundSync.Plugin("my-queue-name", { maxRetentionTime: 86400 })] }), 'PATCH');
